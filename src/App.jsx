a2d10aea324d73c0a9bfe0bf119ea66338bb641d
Scott Mangiapane <scottmangiapane@gmail.com>
2022-09-18 11:32:34 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Dashboard from './Dashboard';
+
+import './App.css';
+
+function App() {
+  return (
+    <div id='app'>
+      <Dashboard />
+      <p>Edit button and sign out</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Dashboard.jsx
@@ -0,0 +1,11 @@
+import './Dashboard.css';
+
+function Dashboard() {
+  return (
+    <div id='dashboard'>
+      <p>dashboard content will go here</p>
+    </div>
+  );
+}
+
+export default Dashboard;
