0a7fd077ae5f436285f5fb599cb3283bfd2e26df
Scott Mangiapane <scottmangiapane@gmail.com>
2022-09-22 11:36:29 -0700

    Made auto-fill dependent on hostname of current tab

--- public/background.js
+++ public/background.js
@@ -1,26 +1,37 @@
-importScripts('./lib/otpauth.umd.min.js');
-
-function getToken(secret) {
-    const totp = new OTPAuth.TOTP({ secret });
-    return totp.generate();
-}
-
-function insertText() {
-    chrome.storage.sync.get(['config'], (result) => {
-        try {
-            const parsedConfig = JSON.parse(result['config']);
-            const token = getToken(parsedConfig[0].secret);
-            chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-                chrome.tabs.sendMessage(tabs[0].id, token);
-            });
-        } catch(err) {
-            console.error(err);
-        }
-    });
-}
+import { TOTP } from './lib/otpauth.esm.min.js';
 
 chrome.commands.onCommand.addListener((command) => {
     if (command === 'insert-text') {
         insertText();
     }
 });
+
+function insertText() {
+    chrome.storage.sync.get(['config'], (result) => {
+        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+            try {
+                const parsedConfig = JSON.parse(result['config']);
+                const matches = parsedConfig.filter((item) => {
+                    const { url } = tabs[0];
+                    return item['auto-fill'].some((host) => doesHostMatch(url, host));
+                });
+                matches.sort((m1, m2) => new Date(m1.lastUsed).compare(new Date(m2.lastUsed)));
+                const token = getToken(matches[0].secret);
+                chrome.tabs.sendMessage(tabs[0].id, token);
+            } catch(err) {
+                console.error(err);
+            }
+        });
+    });
+}
+
+function doesHostMatch(url, expectedHost) {
+    const exp = /^(?:https?:\/\/)?([^\/?\n]+)/;
+    const [, actualHost] = exp.exec(url);
+    return expectedHost === actualHost;
+}
+
+function getToken(secret) {
+    const totp = new TOTP({ secret });
+    return totp.generate();
+}
--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,10 @@ function App() {
     const exampleConfig = '[\n'
     + '  {\n'
     + '    "name": "Example",\n'
-    + '    "secret": "JBSWY3DPEHPK3PXP"\n'
+    + '    "secret": "JBSWY3DPEHPK3PXP",\n'
+    + '    "auto-fill": [\n'
+    + '      "example.com"\n'
+    + '    ]\n'
     + '  }\n'
     + ']\n';
 
